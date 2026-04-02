6d6a629348719a8e8c2cf77a096121fc8b2549a6
Juliana Jaime <27710492+peppermint-juli@users.noreply.github.com>
2026-04-02 18:26:27 -0400

    Add FM Transmitter ZeroMQ implementation with GUI support

---
+++ usrp/fm_transmiter_zmq.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Transmitter
+# Author: julianajaime
+# GNU Radio version: 3.10.12.0
+
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import uhd
+import time
+from gnuradio import zeromq
+from xmlrpc.server import SimpleXMLRPCServer
+import threading
+
+
+
+
+class fm_transmiter_zmq(gr.top_block):
+
+    def __init__(self, freq=426.5625e6):
+        gr.top_block.__init__(self, "Transmitter", catch_exceptions=True)
+        self.flowgraph_started = threading.Event()
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.freq = freq
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.tx_gain = tx_gain = 0
+        self.server_port = server_port = 30000
+        self.server_address = server_address = "172.16.1.202"
+        self.samp_rate_0 = samp_rate_0 = 1e6
+        self.samp_rate = samp_rate = 32000
+        self.lpf_decim = lpf_decim = 5
+        self.audio_samp_rate = audio_samp_rate = 48e3
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.zeromq_pull_source_0 = zeromq.pull_source(gr.sizeof_gr_complex, 1, 'tcp://*:9998', 100, False, (-1), True)
+        self.xmlrpc_server_0_0 = SimpleXMLRPCServer((str(server_address), int(server_port)), allow_none=True)
+        self.xmlrpc_server_0_0.register_instance(self)
+        self.xmlrpc_server_0_0_thread = threading.Thread(target=self.xmlrpc_server_0_0.serve_forever)
+        self.xmlrpc_server_0_0_thread.daemon = True
+        self.xmlrpc_server_0_0_thread.start()
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='fc32',
+                channels=list(range(0,1)),
+            ),
+            "",
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_sink_0.set_center_freq(freq, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_sink_0.set_gain(tx_gain, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.zeromq_pull_source_0, 0), (self.uhd_usrp_sink_0, 0))
+
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+        self.uhd_usrp_sink_0.set_gain(self.tx_gain, 0)
+
+    def get_server_port(self):
+        return self.server_port
+
+    def set_server_port(self, server_port):
+        self.server_port = server_port
+
+    def get_server_address(self):
+        return self.server_address
+
+    def set_server_address(self, server_address):
+        self.server_address = server_address
+
+    def get_samp_rate_0(self):
+        return self.samp_rate_0
+
+    def set_samp_rate_0(self, samp_rate_0):
+        self.samp_rate_0 = samp_rate_0
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+
+    def get_lpf_decim(self):
+        return self.lpf_decim
+
+    def set_lpf_decim(self, lpf_decim):
+        self.lpf_decim = lpf_decim
+
+    def get_audio_samp_rate(self):
+        return self.audio_samp_rate
+
+    def set_audio_samp_rate(self, audio_samp_rate):
+        self.audio_samp_rate = audio_samp_rate
+
+
+
+def argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--freq", dest="freq", type=eng_float, default=eng_notation.num_to_str(float(426.5625e6)),
+        help="Set freq [default=%(default)r]")
+    return parser
+
+
+def main(top_block_cls=fm_transmiter_zmq, options=None):
+    if options is None:
+        options = argument_parser().parse_args()
+    tb = top_block_cls(freq=options.freq)
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+    tb.flowgraph_started.set()
+
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
